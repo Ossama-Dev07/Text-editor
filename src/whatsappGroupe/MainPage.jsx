2b9ec1ab546909540f6e797e0f214f8aa512c991
Ossama-Dev07 <ossamadev22@gmail.com>
2025-02-05 18:40:13 +0100

    fixe UI problems

--- src/whatsappGroupe/MainPage.jsx
+++ src/whatsappGroupe/MainPage.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import isHotkey from "is-hotkey";
 import { Editable, withReact, useSlate, Slate } from "slate-react";
 import {
@@ -8,7 +8,7 @@ import {
   Element as SlateElement,
 } from "slate";
 import { withHistory } from "slate-history";
-import { Button, Icon, Toolbar } from "./EditorUI/index";
+import { Button } from "@/components/ui/button"; // Import shadcn Button
 import {
   AlignCenter,
   AlignJustify,
@@ -19,62 +19,103 @@ import {
   Heading1,
   Heading2,
   Italic,
-  ItalicIcon,
   List,
   ListOrdered,
   Quote,
   Underline,
 } from "lucide-react";
+import { Tabs, TabsContent } from "@/components/ui/tabs";
+import { Separator } from "@/components/ui/separator";
+import { CounterClockwiseClockIcon } from "@radix-ui/react-icons";
+import { PresetSelector } from "./components/preset-selector";
+import { PresetSave } from "./components/preset-save";
+import { CodeViewer } from "./components/code-viewer";
+import { PresetShare } from "./components/preset-share";
+import { PresetActions } from "./components/preset-actions";
+import { presets } from "./data/presets";
+import {
+  FontSizeSelect,
+  FontFamilySelect,
+} from "./EditorUI/font-select-components";
 
 const HOTKEYS = {
-  "mod+b": "bold",
-  "mod+i": "italic",
-  "mod+u": "underline",
-  "mod+`": "code",
+  "ctrl+b": "bold",
+  "ctrl+i": "italic",
+  "ctrl+u": "underline",
+  "ctrl+`": "code",
 };
 const LIST_TYPES = ["numbered-list", "bulleted-list"];
 const TEXT_ALIGN_TYPES = ["left", "center", "right", "justify"];
-const RichTextExample = () => {
+
+const MainPage = () => {
   const renderElement = useCallback((props) => <Element {...props} />, []);
   const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
   const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+
   return (
-    <Slate editor={editor} initialValue={initialValue}>
-      <Toolbar className="w-full">
-        <MarkButton format="bold" icon={<Bold />} />
-        <MarkButton format="italic" icon={<ItalicIcon />} />
-        <MarkButton format="underline" icon={<Underline />} />
-        <MarkButton format="code" icon={<Code />} />
-        <BlockButton format="heading-one" icon={<Heading1 />} />
-        <BlockButton format="heading-two" icon={<Heading2 />} />
-        <BlockButton format="block-quote" icon={<Quote />} />
-        <BlockButton format="numbered-list" icon={<ListOrdered />} />
-        <BlockButton format="bulleted-list" icon={<List />} />
-        <BlockButton format="left" icon={<AlignLeft />} />
-        <BlockButton format="center" icon={<AlignCenter />} />
-        <BlockButton format="right" icon={<AlignRight />} />
-        <BlockButton format="justify" icon={<AlignJustify />} />
-      </Toolbar>
-      <Editable
-        className="h-[400px]"
-        renderElement={renderElement}
-        renderLeaf={renderLeaf}
-        placeholder="Enter some rich text…"
-        spellCheck
-        autoFocus
-        onKeyDown={(event) => {
-          for (const hotkey in HOTKEYS) {
-            if (isHotkey(hotkey, event)) {
-              event.preventDefault();
-              const mark = HOTKEYS[hotkey];
-              toggleMark(editor, mark);
-            }
-          }
-        }}
-      />
-    </Slate>
+    <div>
+      <div
+        className="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16"
+        dir="rtl"
+      >
+        <h2 className="text-lg font-semibold">Playground</h2>
+        <div className="ml-auto flex w-full space-x-2 sm:justify-end">
+          <PresetSelector presets={presets} />
+          <PresetSave />
+          <div className="space-x-2 md:flex">
+            <CodeViewer />
+            <PresetShare />
+          </div>
+          <PresetActions />
+        </div>
+      </div>
+      <Separator />
+      <Tabs>
+        <Slate editor={editor} initialValue={initialValue}>
+          <div className="h-full py-6">
+            <div className="grid h-full gap-10 md:flex ">
+              <div className="md:order-2">
+                <Editable
+                  className="h-[600px] mr-[10px] w-[50%]"
+                  renderElement={renderElement}
+                  renderLeaf={renderLeaf}
+                  placeholder="Enter some rich text…"
+                  spellCheck
+                  autoFocus
+                  onKeyDown={(event) => {
+                    for (const hotkey in HOTKEYS) {
+                      if (isHotkey(hotkey, event)) {
+                        event.preventDefault();
+                        const mark = HOTKEYS[hotkey];
+                        toggleMark(editor, mark);
+                      }
+                    }
+                  }}
+                />
+              </div>
+              <div className="flex- space-y-4 sm:flex md:order-1 ">
+                <MarkButton format="bold" icon={<Bold />} />
+                <MarkButton format="italic" icon={<Italic />} />
+                <MarkButton format="underline" icon={<Underline />} />
+                <MarkButton format="code" icon={<Code />} />
+                <BlockButton format="heading-one" icon={<Heading1 />} />
+                <BlockButton format="heading-two" icon={<Heading2 />} />
+                <BlockButton format="block-quote" icon={<Quote />} />
+                <BlockButton format="numbered-list" icon={<ListOrdered />} />
+                <BlockButton format="bulleted-list" icon={<List />} />
+                <BlockButton format="left" icon={<AlignLeft />} />
+                <BlockButton format="center" icon={<AlignCenter />} />
+                <BlockButton format="right" icon={<AlignRight />} />
+                <BlockButton format="justify" icon={<AlignJustify />} />
+              </div>
+            </div>
+          </div>
+        </Slate>
+      </Tabs>
+    </div>
   );
 };
+
 const toggleBlock = (editor, format) => {
   const isActive = isBlockActive(
     editor,
@@ -106,6 +147,7 @@ const toggleBlock = (editor, format) => {
     Transforms.wrapNodes(editor, block);
   }
 };
+
 const toggleMark = (editor, format) => {
   const isActive = isMarkActive(editor, format);
   if (isActive) {
@@ -114,6 +156,7 @@ const toggleMark = (editor, format) => {
     Editor.addMark(editor, format, true);
   }
 };
+
 const isBlockActive = (editor, format, blockType = "type") => {
   const { selection } = editor;
   if (!selection) return false;
@@ -128,15 +171,14 @@ const isBlockActive = (editor, format, blockType = "type") => {
   );
   return !!match;
 };
+
 const isMarkActive = (editor, format) => {
   const marks = Editor.marks(editor);
   return marks ? marks[format] === true : false;
 };
-const Element = ({ attributes, children, element }) => {
-  // Base styles for all elements
-  const baseStyle = { textAlign: element.align };
 
-  // Styles for specific element types
+const Element = ({ attributes, children, element }) => {
+  const baseStyle = { textAlign: element.align };
   const styles = {
     "block-quote": {
       ...baseStyle,
@@ -174,16 +216,14 @@ const Element = ({ attributes, children, element }) => {
       fontSize: "16px",
     },
   };
-
-  // Get the appropriate style for the element type
   const elementStyle = styles[element.type] || styles.default;
-
   return (
     <div style={elementStyle} {...attributes}>
       {children}
     </div>
   );
 };
+
 const Leaf = ({ attributes, children, leaf }) => {
   if (leaf.bold) {
     children = <strong>{children}</strong>;
@@ -199,15 +239,13 @@ const Leaf = ({ attributes, children, leaf }) => {
   }
   return <span {...attributes}>{children}</span>;
 };
+
 const BlockButton = ({ format, icon }) => {
   const editor = useSlate();
   return (
     <Button
-      active={isBlockActive(
-        editor,
-        format,
-        TEXT_ALIGN_TYPES.includes(format) ? "align" : "type"
-      )}
+      variant={isBlockActive(editor, format) ? "default" : "outline"} // shadcn button styles
+      size="sm"
       onMouseDown={(event) => {
         event.preventDefault();
         toggleBlock(editor, format);
@@ -217,11 +255,13 @@ const BlockButton = ({ format, icon }) => {
     </Button>
   );
 };
+
 const MarkButton = ({ format, icon }) => {
   const editor = useSlate();
   return (
     <Button
-      active={isMarkActive(editor, format)}
+      variant={isMarkActive(editor, format) ? "default" : "outline"} // shadcn button styles
+      size="sm"
       onMouseDown={(event) => {
         event.preventDefault();
         toggleMark(editor, format);
@@ -231,6 +271,7 @@ const MarkButton = ({ format, icon }) => {
     </Button>
   );
 };
+
 const initialValue = [
   {
     type: "paragraph",
@@ -266,4 +307,5 @@ const initialValue = [
     children: [{ text: "Try it out for yourself!" }],
   },
 ];
-export default RichTextExample;
+
+export default MainPage;
